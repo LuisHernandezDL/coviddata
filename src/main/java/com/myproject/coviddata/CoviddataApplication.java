f1332e8d24bb2ac9727883033f2d4153b6c117ca
Luis Hernandez <lhernandezd12@miumg.edu.gt>
2025-04-25 21:41:18 -0600

    Configuracion del Pom.xml y application.properties

--- src/main/java/com/myproject/coviddata/CoviddataApplication.java
+++ src/main/java/com/myproject/coviddata/CoviddataApplication.java
@@ -2,12 +2,13 @@ package com.myproject.coviddata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoviddataApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoviddataApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CoviddataApplication.class, args);
+    }
 }
